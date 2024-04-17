811020f8baaa376796459e8c4f00389cae64838c
Your Name <you@example.com>
2024-04-17 01:28:21 -0400

    m

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,5 +1,5 @@
 # Uncomment the imports before you add the code
-
+from .restapis import get_request, analyze_review_sentiments, post_review
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
@@ -7,7 +7,11 @@ from django.urls import path
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-
+    path(route='add_review', view=views.add_review, name='add_review'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for login
      path(route='get_cars', view=views.get_cars, name ='getcars'),
      path(route='registration',view= views.registration, name='registration'),
